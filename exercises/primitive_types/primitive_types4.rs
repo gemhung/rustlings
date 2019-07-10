24a10d257a6c46641c2a58d1e7af598524721af1
chihhan.hung <chihhan.hung@sbibits.com>
2019-07-10 15:56:40 +0900

    2019/7/10

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -5,12 +5,12 @@
 fn main() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = [2, 3, 4]; 
 
     if nice_slice == [2, 3, 4] {
         println!("Nice slice!");
     } else {
-        println!("Not quite what I was expecting... I see: {:?}", nice_slice);
+          println!("Not quite what I was expecting... I see: {:?}", nice_slice);
     }
 }
 
